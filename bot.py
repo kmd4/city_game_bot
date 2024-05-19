3b7ff851fcd87f04369cab1b3a6f163fe0dc401e
Maria Kutyanova <86561716+kmd4@users.noreply.github.com>
2024-05-19 21:10:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,1 @@
-import asyncio
-from src.constants import *
-from handlers.handlers import router
-from aiogram import Bot, Dispatcher
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot)
-
-
-async def main():
-    dp.include_router(router)
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
